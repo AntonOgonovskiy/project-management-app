c77acd20f511e74aaee4f488f9c1c222d684ccfa
Anton <antonogonovskiy@gmail.com>
2022-11-29 21:40:08 +0300

    refactor

--- my-app/src/Pages/BoardPage.tsx
+++ my-app/src/Pages/BoardPage.tsx
@@ -1,22 +1,40 @@
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getBoard } from "../API/api";
 import { BoardData } from "../types";
+import ReplyIcon from "@mui/icons-material/Reply";
+import { Button } from "@mui/material";
+import { useEffect, useState } from "react";
 
 const BoardPage = () => {
+  const dispatch = useDispatch();
+  const [boardData, setData] = useState({});
   const data = useSelector((state: BoardData) => state.boardData.data);
   const id: string = data.owner
     ? (data.owner as string)
     : (localStorage.getItem("boardData") as string);
 
   const getData = async (id: string) => {
-    await getBoard(id);
+    dispatch({ type: "ONLOAD", payload: true });
+    const resp = await getBoard(id);
+    setData(resp);
+    dispatch({ type: "LOADED", payload: false });
   };
-
   useEffect(() => {
-    console.log(getData(id));
-  });
-  return <div></div>;
+    getData(id);
+  }, []);
+  console.log(boardData);
+
+  return (
+    <div className="boardPageWrapper">
+      <div className="boardHeader">
+        <Button variant="contained" startIcon={<ReplyIcon />}>
+          Back
+        </Button>
+        <span>{}</span>
+      </div>
+      <div className="boardColumns"></div>
+    </div>
+  );
 };
 
 export default BoardPage;
