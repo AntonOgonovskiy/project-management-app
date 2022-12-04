de6f8100336a678c2c8ff1e56776340057df45c4
Anton <antonogonovskiy@gmail.com>
2022-12-05 00:26:32 +0300

    feat: add sticky header with animation

--- my-app/src/Components/Header/Header.tsx
+++ my-app/src/Components/Header/Header.tsx
@@ -8,10 +8,21 @@ import CreateBoard from "../CreateBoardButton/CreateBoard";
 import { useDispatch } from "react-redux";
 import { getUserBoards } from "../../API/api";
 import { GetId } from "../../Utils/utils";
+import { useEffect, useRef, useState } from "react";
 
 const Header = () => {
+  const [sticky, setSticky] = useState({ isSticky: false, offset: 0 });
+  const headerRef = useRef<HTMLInputElement>(null);
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const handleScroll = (elTopOffset: number, elHeight: number) => {
+    if (window.pageYOffset > elTopOffset + elHeight) {
+      setSticky({ isSticky: true, offset: elHeight });
+    } else {
+      setSticky({ isSticky: false, offset: 0 });
+    }
+  };
 
   const logOut = () => {
     localStorage.clear();
@@ -19,12 +30,33 @@ const Header = () => {
     dispatch({ type: "LOGIN", payload: "" });
     navigate("/");
   };
+
   const getBoards = async () => {
     const boards = await getUserBoards(GetId());
     dispatch({ type: "BOARD", payload: boards });
   };
+
+  useEffect(() => {
+    const header = headerRef.current;
+    if (header) {
+      const headerPosition = header.getBoundingClientRect();
+      const handleScrollEvent = () => {
+        handleScroll(headerPosition.top, headerPosition.height);
+      };
+      window.addEventListener("scroll", handleScrollEvent);
+      return () => {
+        window.removeEventListener("scroll", handleScrollEvent);
+      };
+    }
+  }, []);
+
   return (
-    <header className="headerWrapper">
+    <header
+      ref={headerRef}
+      className={
+        sticky.isSticky ? "headerWrapper headerSticky" : "headerWrapper "
+      }
+    >
       <Link className="homeLink" to="/">
         <Button variant="contained">Home</Button>
       </Link>
